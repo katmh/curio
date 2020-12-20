602691fc16eaad08b156fd4b4c6f692b3ce5e964
Katherine Huang <hello@katmh.com>
2020-12-20 11:40:13 -0500

    Add About

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,10 +1,31 @@
 <header class="container">
 	<h1><a href="/">Curio</a></h1>
-	<p class="tagline">
-		<a href="https://katmh.com">Kat</a>'s technical blog
-	</p>
+	<p class="tagline"><a href="https://katmh.com">Kat</a>'s technical blog</p>
 </header>
 
 <section class="container">
-	<slot></slot>
+	<slot />
 </section>
+
+<footer class="container">
+	<p class="about">
+		<b>About:</b>
+		Hi, I’m Kat and this is where I write casually and (I hope) legibly about
+		programming projects and learnings. I mostly do frontend web development and
+		data visualization. I also take computer science theory and practice courses
+		at MIT, where I’m majoring in comparative media, and I’m participating in
+		<a href="https://recurse.com">Recurse Center</a>
+		in January 2021. I’ve done software-y things for jobs before, but coding
+		first came into my life as a hobby —
+		<a href="https://www.katmh.com/girl-in-stem">nurtured</a>
+		by Tumblr,
+		<a href="https://mahacks.com">hackathons</a>, and
+		<a href="https://hackclub.com">hackathon</a>
+		<a href="https://aspirations.org">communities</a>
+		— and I enjoy approaching it with levity. “Curio” is an imperfect, snappy
+		synonym for “knickknack,” which is what I (unabashedly) consider many of my
+		side
+		<a href="https://katmh.com/projects">projects</a>
+		to be. I hope you find something helpful or insightful on these pages :)
+	</p>
+</footer>
