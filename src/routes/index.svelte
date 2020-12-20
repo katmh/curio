273834b78184a746eb567b9c9cc241e439eb6c9a
Katherine Huang <hello@katmh.com>
2020-12-20 13:16:59 -0500

    No prefetch for now :(

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,24 +9,6 @@
 </script>
 
 <script>
-	import { onMount } from "svelte";
-	let Prism;
-	let code = "let b = 4;";
-	onMount(async () => {
-		// Load the prismjs first after the page is loaded
-		const prismModule = await import("svelte-prismjs");
-		await import("prismjs/components/prism-c.js");
-		await import("prism-svelte");
-		await import("prismjs/plugins/line-highlight/prism-line-highlight.js");
-		await import("prismjs/plugins/file-highlight/prism-file-highlight.js");
-		// Once everything is loaded load the prismjs module
-		Prism = prismModule.default;
-		setInterval(() => {
-			code = code == "let b = 4;" ? "let c = 323;" : "let b = 4;";
-			console.log("should notice change");
-		}, 3000);
-	});
-
 	export let posts;
 </script>
 
@@ -43,7 +25,7 @@
 				for the page as soon as the user hovers over the link
 				or taps it, instead of waiting for the 'click' event -->
 		<h2 class="post_title">
-			<a rel="prefetch" href="/{post.slug}">{post.title}</a>
+			<a rel="external" href="/{post.slug}">{post.title}</a>
 		</h2>
 		<p class="date">{post.date}</p>
 		<p class="summary">{post.summary}</p>
